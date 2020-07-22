3799a92a42d269fc403408c5a745630346ef6af6
Romario <romaef23@gmail.com>
2020-07-22 12:02:59 -0500

    I add logo svg code for react

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 
 export const App = () => (
 	<div>
 		{/* <p>puto</p> */}
 		<GlobalStyle />
+		<Logo />
 		<ListOfCategories />
 		<ListOfPhotoCards />
 	</div>
