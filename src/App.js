6bcfef79698a5685c60767fca4ab9877b7b24a0d
Romario <romaef23@gmail.com>
2020-07-22 11:46:09 -0500

    I add a list of photo cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+
 
 export const App = () => (
 	<div>
 		{/* <p>puto</p> */}
 		<GlobalStyle />
 		<ListOfCategories />
+		<ListOfPhotoCards />
 	</div>
 )
