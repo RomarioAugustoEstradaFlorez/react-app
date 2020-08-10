7ad08afe3d7d39b8f82bdf8d8d2b319cb33809e2
Romario <romaef23@gmail.com>
2020-08-10 16:54:19 -0500

    Adding the custom hooks on a folder to separate them
    This is usefull because them will works in other components

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,54 +1,17 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react'
+import React, { Fragment } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useNearItemsToShow } from '../../hooks/useNearItemsToShow'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-	const element = useRef(null);
-	const keyLike = 'like-' + id;
-	const [show, setShow] = useState(false)
-
-	const [liked, pushLiked] = useState(() => {
-		try {
-			const like = window.localStorage.getItem(keyLike);
-			return JSON.parse(like);
-		} catch (error) {
-			return false;
-			console.error('The localstorage dont works on incognit tabs: ', error)
-		}
-		return false;
-	})
+	const [show, element] = useNearItemsToShow() // this get for the element instead
+	const keyLike = 'like-' + id
+	const [liked, pushLiked] = useLocalStorage(keyLike, false) // this shit is awesome jaja, I love the custom hooks
 
 	const Icon = liked ? MdFavorite : MdFavoriteBorder; // Uppercase for react, because it have to know if the render is a component
-
-	useEffect(function () {
-		Promise.resolve(
-			typeof window.IntersectionObserver !== 'undefined'
-				? window.IntersectionObserver
-				: import('intersection-observer')
-		).then(() => {
-			const observer = new window.IntersectionObserver(function (entries) {
-				const { isIntersecting } = entries[0]
-				if (isIntersecting) {
-					setShow(true)
-					observer.disconnect()
-				}
-			})
-			observer.observe(element.current)
-		})
-	}, [element])
-
-
-	const SetLocalLiked = val => {
-		try {
-			window.localStorage.setItem(keyLike, val)
-		} catch (error) {
-			console.error('The localstorage dont works on incognit tabs: ', error)
-		}
-		pushLiked(val);
-	}
 
 	return (
 		<Article ref={element}>
@@ -60,10 +23,10 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 						</ImgWrapper>
 					</a>
 
-					<Button onClick={() => SetLocalLiked(!liked)}>
-
+					<Button onClick={() => pushLiked(!liked)}>
 						<Icon size='32px' /> {likes} likes!
           </Button>
+
 				</Fragment>
 			}
 		</Article>
