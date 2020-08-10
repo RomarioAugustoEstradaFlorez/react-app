00785b569d68d66006c2e72928d47afe1fccdc1f
Romario <romaef23@gmail.com>
2020-08-10 16:17:53 -0500

    Adding liked with localstorage
    using states and parameters from the photocard list

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,13 +1,27 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-	const element = useRef(null)
+	const element = useRef(null);
+	const keyLike = 'like-' + id;
 	const [show, setShow] = useState(false)
+
+	const [liked, pushLiked] = useState(() => {
+		try {
+			const like = window.localStorage.getItem(keyLike);
+			return JSON.parse(like);
+		} catch (error) {
+			return false;
+			console.error('The localstorage dont works on incognit tabs: ', error)
+		}
+		return false;
+	})
+
+	const Icon = liked ? MdFavorite : MdFavoriteBorder; // Uppercase for react, because it have to know if the render is a component
 
 	useEffect(function () {
 		Promise.resolve(
@@ -27,6 +41,15 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 	}, [element])
 
 
+	const SetLocalLiked = val => {
+		try {
+			window.localStorage.setItem(keyLike, val)
+		} catch (error) {
+			console.error('The localstorage dont works on incognit tabs: ', error)
+		}
+		pushLiked(val);
+	}
+
 	return (
 		<Article ref={element}>
 			{
@@ -37,8 +60,9 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 						</ImgWrapper>
 					</a>
 
-					<Button>
-						<MdFavoriteBorder size='32px' /> {likes} likes!
+					<Button onClick={() => SetLocalLiked(!liked)}>
+
+						<Icon size='32px' /> {likes} likes!
           </Button>
 				</Fragment>
 			}
