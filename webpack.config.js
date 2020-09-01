5489593ebd961578b8f941a5c2935055af61fa29
Romario <romaef23@gmail.com>
2020-09-01 13:28:12 -0500

    Adding PWA requeriments usin webpackmanifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const path = require('path')
 
 module.exports = {
   output: {
@@ -8,6 +10,42 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifest({
+      name: 'Pettagram - Tu app social de mascotas',
+      short_name: 'Pettagram',
+      description: 'Con Petgram puedes encontrar y subir fotos de animales domésticos.',
+      background_color: '#ffffff',
+      theme_color: '#2196f3',
+      crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+      ios: {
+        'apple-mobile-web-app-title': 'Pettagram',
+        'apple-mobile-web-app-status-bar-style': '2196f3'
+      },
+      icons: [
+        {
+          src: path.resolve('src/assets/img/icon.png'),
+          sizes: [120, 152, 167, 180, 1024],
+          destination: path.join('icons', 'ios'),
+          ios: true
+        },
+        {
+          src: path.resolve('src/assets/img/icon.png'),
+          size: 1024,
+          destination: path.join('icons', 'ios'),
+          ios: 'startup'
+        },
+        {
+          src: path.resolve('src/assets/img/icon.png'),
+          sizes: [36, 48, 72, 96, 144, 192, 512],
+          destination: path.join('icons', 'android')
+        },
+        {
+          src: path.resolve('src/assets/img/icon.png'),
+          size: '1024x1024',
+          purpose: 'maskable'
+        }
+      ]
     })
   ],
   module: {
